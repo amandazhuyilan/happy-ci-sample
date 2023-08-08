e29d7b0eb2eaa45f49a879053c90bcfcd98c6a1d
Amanda Zhu <amandazhuyilan@gmail.com>
2023-08-08 14:27:15 +0900

    remove unused for loop and test GHA

--- simple_example.cpp
+++ simple_example.cpp
@@ -8,9 +8,5 @@ int main() {
     std::cout << "Simple example C++ compiled correctly and ran." << std::endl;
     std::cout << simple_lib_function() << std::endl;
 
-    for (int i = 0; i < 10; i++)
-    {
-        std::cout << "value of i is: " << i << std::endl;
-    }
     return 0;
 }
